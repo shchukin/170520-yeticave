3d43583455a54c74944d5c750e2cd0f68728b4eb
Anton Shchukin <a.a.shchukin@gmail.com>
2018-09-16 01:40:35 +0700

    Используем массив категорий в блоке промок

--- index.php
+++ index.php
@@ -58,10 +58,11 @@ $cats = ["Доски и лыжи", "Крепления", "Ботинки", "Од
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
+            <?php foreach ($cats as $key => $val): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="#"><?= $val ?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
