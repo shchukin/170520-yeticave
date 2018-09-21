b7fd2a4c1fcd1ebbc2ceacda2c81bd53c6bd13b6
Anton Shchukin <a.a.shchukin@gmail.com>
2018-09-22 01:49:46 +0700

    Шаблонизируем layout и главную страницу

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+function renderTemplate($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require($name);
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+
+function cleanUpPrice($value)
+{
+    $value = ceil($value);
+    if ($value >= 1000) {
+        $value = number_format($value, 0, '.', ' ');
+    }
+    $value = $value . '<b class="rub">р</b>';
+    return $value;
+}
---
+++ templates/index.php
@@ -0,0 +1,43 @@
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и
+        горнолыжное снаряжение.</p>
+    <ul class="promo__list">
+        <?php foreach ($cats as $key => $val): ?>
+            <li class="promo__item promo__item--<?= $val['alias'] ?>">
+                <a class="promo__link" href="#"><?= $val['name'] ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
+        <?php foreach ($lots as $key => $val): ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?= $val['preview'] ?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?= $cats[$val['cat_id']]['name'] ?></span>
+                    <h3 class="lot__title">
+                        <a class="text-link" href="pages/lot.html">
+                            <?= htmlspecialchars($val['title']) ?>
+                        </a>
+                    </h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?= cleanUpPrice($val['price']) ?></span>
+                        </div>
+                        <div class="lot__timer timer">
+
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</section>
