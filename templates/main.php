7961373a4e43c5cb3fb31b8c649c469ab7c9fc63
Anton Shchukin <a.a.shchukin@gmail.com>
2018-09-22 13:17:09 +0700

    Добавляем лидирующие нули

--- templates/main.php
+++ templates/main.php
@@ -33,7 +33,9 @@
                             <span class="lot__cost"><?= cleanUpPrice($val['price']) ?></span>
                         </div>
                         <div class="lot__timer timer">
-                            <?= floor($lotExpirationTime / 60 / 60 ) ?> : <?= floor( $lotExpirationTime / 60 ) % 60  ?>
+                            <?= str_pad(floor($lotExpirationTime/60/60), 2, '0', STR_PAD_LEFT); ?>
+                            :
+                            <?= str_pad(floor($lotExpirationTime/60)%60, 2, '0', STR_PAD_LEFT); ?>
                         </div>
                     </div>
                 </div>
