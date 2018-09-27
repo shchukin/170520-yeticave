86e02255166dccb4f30c13f22d8988f19505ba6b
Anton Shchukin <a.a.shchukin@gmail.com>
2018-09-27 09:57:29 +0700

    Запрос добавления категорий

---
+++ queries.sql
@@ -0,0 +1,24 @@
+-- INSERT INTO
+--   `category`
+-- SET
+--     `alias` = 'boards', `name` = 'Доски и лыжи',
+--     `alias` = 'attachment', `name` = 'Крепления',
+--     `alias` = 'boots', `name` = 'Ботинки',
+--     `alias` = 'clothing', `name` = 'Одежда',
+--     `alias` = 'tools', `name` = 'Инструменты',
+--     `alias` = 'other', `name` = 'Разное';
+
+
+
+INSERT INTO
+  `category` (`alias`, `name`)
+VALUES
+    ('boards', 'Доски и лыжи'),
+    ('attachment', 'Крепления'),
+    ('boots', 'Ботинки'),
+    ('clothing', 'Одежда'),
+    ('tools', 'Инструменты'),
+    ('other', 'Разное')
+;
+
+
