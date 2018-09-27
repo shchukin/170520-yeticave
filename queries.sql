599b307715d6ab6d9554e6a5b99be92c46c382a8
Anton Shchukin <a.a.shchukin@gmail.com>
2018-09-27 13:32:44 +0700

    Запросы на количество ставок и на текущую цену к лотам

--- queries.sql
+++ queries.sql
@@ -118,6 +118,11 @@ WHERE `winner_id` IS NULL ORDER BY `creation_date` DESC LIMIT 3;
 
 
 
-# Попытка вытащить лот с количеством ставок к нему
-#SELECT `title`, `image`, FROM `lot`;
-#SELECT COUNT(`bid_id`) FROM `bid` WHERE `lot_id` = 1
+# лоты с количеством ставок к ним
+SELECT `title`, `image`, COUNT(`b`.`bid_id`) FROM `lot` `l`
+JOIN `bid` `b` ON `b`.`lot_id` = `l`.`lot_id` GROUP BY `b`.`lot_id`
+
+
+# лоты с текущей ценой
+SELECT `title`, `image`, GREATEST( COALESCE(MAX(`b`.`value`), 0) , `l`.`price` ) FROM `lot` `l`
+LEFT JOIN `bid` `b` ON `b`.`lot_id` = `l`.`lot_id` GROUP BY `b`.`lot_id`
