d50f3ae007a81f1de78619f22a8f5ce4fc3d9231
Anton Shchukin <a.a.shchukin@gmail.com>
2018-10-04 00:58:35 +0700

    Удаляем массив ставок

--- index.php
+++ index.php
@@ -9,47 +9,6 @@ $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
 
 
-$lots = [
-    [
-        'title' => '2014 Rossignol District Snowboard',
-        'cat_id' => 0,
-        'price' => 10999,
-        'preview' => 'img/lot-1.jpg'
-    ],
-    [
-        'title' => 'DC Ply Mens 2016/2017 Snowboard',
-        'cat_id' => 0,
-        'price' => 159999,
-        'preview' => 'img/lot-2.jpg'
-    ],
-    [
-        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'cat_id' => 1,
-        'price' => 8000,
-        'preview' => 'img/lot-3.jpg'
-    ],
-    [
-        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'cat_id' => 2,
-        'price' => 10999,
-        'preview' => 'img/lot-4.jpg'
-    ],
-    [
-        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'cat_id' => 3,
-        'price' => 7500,
-        'preview' => 'img/lot-5.jpg'
-    ],
-    [
-        'title' => 'Маска Oakley Canopy',
-        'cat_id' => 5,
-        'price' => 5400,
-        'preview' => 'img/lot-6.jpg'
-    ]
-];
-
-
-
 require('init.php');
 
 
