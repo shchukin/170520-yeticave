fbc49a3b98f35ff14f5a63fca5bcbc8f8793cacb
Anton Shchukin <a.a.shchukin@gmail.com>
2018-10-05 19:07:08 +0700

    Черновик обработки формы

--- add.php
+++ add.php
@@ -2,27 +2,10 @@
 
 require('init.php');
 
-$lot_id = isset($_GET['lot_id']) ? intval($_GET['lot_id']) : 0;
+
+/* Принимаем данные здесь? */
 
 
-/* Вытаскиваем лот */
-
-//$lot = [];
-//
-//if ($lot_id) {
-//    $sql = "SELECT `title`, `description`, `image`, `expiry_date`, `step`, `c`.`name` AS `category`, `winner_id` FROM `lot` `l` JOIN `category` `c` ON `l`.`category_id` = `c`.`category_id` WHERE `lot_id` = " . $lot_id;
-//    $result = mysqli_query($con, $sql);
-//    if ($result) {
-//        $lot = mysqli_fetch_array($result, MYSQLI_ASSOC);
-//    }
-//}
-//
-//if (!$lot) {
-//    http_response_code(404);
-//    $page = renderTemplate('error.php', ['error' => 'Лот не существует']);
-//    print($page);
-//    exit();
-//}
 
 
 
--- templates/add.php
+++ templates/add.php
@@ -1,4 +1,4 @@
-<form class="form form--add-lot container form--invalid" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+<form class="form form--add-lot container form--invalid" action="add.php" method="post"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
         <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
